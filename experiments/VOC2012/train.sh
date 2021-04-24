6ca73e5a3dee591533548d7f6fbddb3b22b6ced3
bhaveshbaranda <bhaveshbaranda6@gmail.com>
2021-04-24 18:43:29 +0530

    Update train.sh

--- experiments/VOC2012/train.sh
+++ experiments/VOC2012/train.sh
@@ -3,5 +3,5 @@
 work_path=$(dirname $0)
 cd ./${work_path}
 
-python ./train_voca.py --network resnet50_v1d --dataset voca --gpus 0 --epochs 25 \
+python ./train_voca.py --network resnet50_v1d --dataset voca --gpus 0 --epochs 14 \
         --resume /kaggle/working/horelation_resnet50_v1d_voca_0012_0.8818.params --start-epoch 13 --max-lr 3e-5 --min-lr 1e-6 --cycle-len 30000 --seed 233 --verbose
