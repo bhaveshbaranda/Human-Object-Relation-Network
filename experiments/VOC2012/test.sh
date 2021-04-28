a40029156661dbe38c7db18946a5d5ed5277f508
bhaveshbaranda <bhaveshbaranda6@gmail.com>
2021-04-28 15:36:43 +0530

    Update test.sh

--- experiments/VOC2012/test.sh
+++ experiments/VOC2012/test.sh
@@ -4,4 +4,4 @@ work_path=$(dirname $0)
 cd ./${work_path}
 
 python ./eval_voca.py --network resnet50_v1d --dataset voca --gpus 0 \
- --pretrained kaggle/working/horelation_resnet50_v1d_voca_0001_0.8852.params --save-outputs
+ --pretrained /kaggle/working/horelation_resnet50_v1d_voca_0001_0.8852.params --save-outputs
